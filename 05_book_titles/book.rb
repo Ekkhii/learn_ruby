f7361016fae43fbaacd774d3b9cf6860fa63b441
Martin Berangere <martin.berangere@live.fr>
2018-04-09 21:55:40 +0200

    Exercice 05 enfin terminé git add .!

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
 # write your code here
+    attr_accessor :title
+
+    def title=(titre)
+        titre_array = titre.split(" ")
+
+        exceptions = ['the', 'a' , 'an' , 'and', 'in', 'of']
+        always = ['i']
+        
+        titre_array.each do |word|
+            unless exceptions.include? word
+                word.capitalize!
+            end
+            
+            if always.include? word
+                word.capitalize
+            end
+            
+        end
+
+        titre_array[0].capitalize!
+
+        titre = titre_array.join(" ")
+
+        @title = titre
+    end
 end
