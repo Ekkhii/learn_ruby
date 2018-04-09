d8631db5fb63ea2657ecaddd57c22308505ac44d
Manu <manujouve@gmail.com>
2018-04-09 18:35:40 +0200

    Exo 4 encours

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+def translate (phrase)
+  alphabet = ('a'..'z').to_a
+  voyelles = %w[a e i o u]
+  consonnes = alphabet - voyelles
+
+  if voyelles.include?(phrase[0])
+    phrase + 'ay'
+  elsif consonnes.include?(phrase[0]) && consonnes.include?(phrase[1])
+    phrase[2..-1] + phrase[0..1] + 'ay'
+  elsif consonnes.include?(phrase[0])
+    phrase[1..-1] + phrase[0] + 'ay'
+  else
+    puts phrase
+  end
+end
