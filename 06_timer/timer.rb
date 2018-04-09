f39853b32f5e25f9860b6769cf2d3d4e598a6821
Martin Berangere <martin.berangere@live.fr>
2018-04-09 23:52:21 +0200

    ALELUIA ON A FINIgit statusgit statusgit statusgit statusgit status

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
+
+  attr_accessor :seconds
   #write your code here
+  
+def initialize
+  @seconds = 0
+end
+
+def time_string
+  
+  heures = seconds / 3600
+  minutes = (seconds/60) % 60
+  secondes = @seconds % 60
+  
+  h = "%02d" % heures
+  m = "%02d" % minutes
+  s = "%02d" % secondes
+  
+   "#{h}:#{m}:#{s}"
+end
 end
