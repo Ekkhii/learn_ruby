d335b4b9bc56915dda5288c5d71960210def5103
Martin Berangere <martin.berangere@live.fr>
2018-04-09 12:21:10 +0200

    exo 00 01 et 02 fait

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,14 @@
 #write your code here
+
+def temperature
+end
+
+def ftoc(farenheit)
+	return(farenheit.to_f-32) * 5.0 / 9.0
+end
+
+def ctof(celsius)
+	return celsius.to_f * 9.0/5.0 + 32 
+end
+
+		
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+
+def calculator
+end
+
+def add(x,y)
+	x.to_i + y.to_i
+end
+
+def subtract(x,y)
+	x.to_i - y.to_i
+end
+
+n = []
+def sum(n)
+	n.sum
+end
+
+#ATTENTION CETTE FONCTIONNALITE FONCTIONNE A PARTIR DE RUBY 2.4.0 !
+#METTEZ VOUS A JOUR SI BESOIN POUR LA CORRECTION
